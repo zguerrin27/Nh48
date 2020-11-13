12d016a6eccc5205e958b8b840900056381a5267
Zachary Guerrin <z.guerrin@gmail.com>
2020-11-13 09:40:05 -0500

    Learning styling

--- App.js
+++ App.js
@@ -17,50 +17,44 @@ import {
  } from 'react-native';
 
 export default function App() {
-  const handleHomepageTouch = () => {console.log("Homepage img pressed")}
-  // console.log(Dimensions.get("screen"))
 
-
-  const dimensions = useDimensions()
-  console.log(dimensions)
 
   return (
-    // <SafeAreaView style={styles.container}>
-    //   <Text>Welcome to NH-48!</Text>
-    //   <Button title="Click Me" onPress={() => Alert.alert("My Title", "My Mesage", [
-    //     { text: "Yes", onPress: () => console.log("YES") },
-    //     { text: "No", onPress: () => console.log("NO") }
-    //   ])}/>
-    //   <TouchableHighlight onPress={handleHomepageTouch}>
-    //     <Image 
-    //       source={require('./assets/Main-WP.jpg')}
-    //       // resizeMode="cover"
-    //       style={{
-    //         height: 500,
-    //         width: 400
-    //       }}
-    //       />
-    //   </TouchableHighlight>
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
 
-    <SafeAreaView>
       <View style={{
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: '60%'
+        backgroundColor: '#fff',
+        flex: 1,
+        flexDirection: "row",  // horizontal 
+        justifyContent: "center", // main
+        alignItems: "center"
       }}>
 
+        <View style={{
+          backgroundColor: 'dodgerblue',
+          height: 100,
+          width: 100
+        }}/>
+        <View style={{
+          backgroundColor: 'gold',
+          height: 100,
+          width: 100
+        }}/>
+        <View style={{
+          backgroundColor: 'tomato',
+          height: 100,
+          width: 100
+        }}/>
+
       </View>
-    </SafeAreaView>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
